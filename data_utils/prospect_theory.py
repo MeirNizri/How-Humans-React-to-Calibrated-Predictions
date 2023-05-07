a97ff6c38fbe2e60e476b1ed10fc81ef81c81609
MeirNizri <maor3412@gmail.com>
2023-05-07 15:32:03 +0300

    update surevy results

--- data_utils/prospect_theory.py
+++ data_utils/prospect_theory.py
@@ -20,17 +20,17 @@ def inverse_probability_weighting(p, alpha=0.71):
     weighted_probabilities = (np.power(p, 1/alpha) / np.power((np.power(p, 1/alpha) + np.power(1-p, 1/alpha)), 1/alpha))
     return np.minimum(weighted_probabilities, 1)
 
-# if __name__ == '__main__':
-#     # Test inverse probability weighting functions for different values of p 
-#     for p in np.arange(0, 1.1, 0.1):
-#         p_hat = probability_weighting(p)
-#         p_hat = inverse_probability_weighting(p_hat)
+if __name__ == '__main__':
+    # Test inverse probability weighting functions for different values of p 
+    for p in np.arange(0, 1.1, 0.1):
+        # p_hat = probability_weighting(p)
+        p_hat = inverse_probability_weighting(p)
 
-#         # p_hat should be equal to p 
-#         print(f"{p:.1f} -> {p_hat:.3f}")
+        # p_hat should be equal to p 
+        print(f"{p:.1f} -> {p_hat:.3f}")
     
-#     random_pred = np.random.random(20)
-#     print(type(random_pred))
-#     print(type(inverse_probability_weighting(random_pred)))
+    # random_pred = np.random.random(20)
+    # print(type(random_pred))
+    # print(type(inverse_probability_weighting(random_pred)))
 
 
